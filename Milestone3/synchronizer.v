363b2f289179b85bae74ae7025462295491a3f7c
hab20034 <125351994+hab20034@users.noreply.github.com>
2024-12-05 16:52:09 +0200

    Update synchronizer.v

--- Milestone3/synchronizer.v
+++ Milestone3/synchronizer.v
@@ -1,1 +1,15 @@
+module synchronizer (
+    input Clk,
+    input SIG,
+    output SIG1
+);
+reg meta;
+reg sig1_reg;
 
+always @(posedge Clk) begin
+    meta <= SIG;
+    sig1_reg <= meta;
+end
+
+assign SIG1 = sig1_reg;
+endmodule
